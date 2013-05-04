9e8bcfb97aac7108ad78ddf8752461e1e9c995a8
f1logic <f1logic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-04 12:41:43 +0000

    social-media-auto-publish 1.1 release
    git-svn-id: https://plugins.svn.wordpress.org/social-media-auto-publish/tags/1.1@707885 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/menu.php
@@ -0,0 +1,47 @@
+<?php
+if ( is_admin() )
+{
+
+	add_action('admin_menu', 'xyz_smap_menu');
+	//add_action('admin_print_styles', 'xyz_smap_admin_style');
+	
+	wp_enqueue_script('jquery');
+	wp_register_script( 'xyz_notice_script', plugins_url('social-media-auto-publish/js/notice.js') );
+	wp_enqueue_script( 'xyz_notice_script' );
+	
+	wp_register_style('xyz_smap_style', plugins_url('social-media-auto-publish/admin/style.css'));
+	wp_enqueue_style('xyz_smap_style');
+
+}
+
+function xyz_smap_menu()
+{
+	add_menu_page('Social Media Auto Publish - Manage settings', 'Social Media Auto Publish', 'manage_options', 'social-media-auto-publish-settings', 'xyz_smap_settings');
+	$page=add_submenu_page('social-media-auto-publish-settings', 'Social Media Auto Publish - Manage settings', ' Settings', 'manage_options', 'social-media-auto-publish-settings' ,'xyz_smap_settings'); // 8 for admin
+	add_submenu_page('social-media-auto-publish-settings', 'Social Media Auto Publish - About', 'About', 'manage_options', 'social-media-auto-publish-about' ,'xyz_smap_about'); // 8 for admin
+}
+
+
+function xyz_smap_settings()
+{
+	require( dirname( __FILE__ ) . '/header.php' );
+	require( dirname( __FILE__ ) . '/settings.php' );
+	require( dirname( __FILE__ ) . '/footer.php' );
+}
+
+
+
+function xyz_smap_about()
+{
+	require( dirname( __FILE__ ) . '/header.php' );
+	require( dirname( __FILE__ ) . '/about.php' );
+	require( dirname( __FILE__ ) . '/footer.php' );
+}
+
+function xyz_smap_admin_style()
+{
+	require( dirname( __FILE__ ) . '/style.php' );
+
+}
+
+?>
---
+++ xyz-functions.php
@@ -0,0 +1,61 @@
+<?php
+
+
+if(!function_exists('xyz_trim_deep'))
+{
+
+	function xyz_trim_deep($value) {
+		if ( is_array($value) ) {
+			$value = array_map('xyz_trim_deep', $value);
+		} elseif ( is_object($value) ) {
+			$vars = get_object_vars( $value );
+			foreach ($vars as $key=>$data) {
+				$value->{$key} = xyz_trim_deep( $data );
+			}
+		} else {
+			$value = trim($value);
+		}
+
+		return $value;
+	}
+
+}
+
+if(!function_exists('esc_textarea'))
+{
+	function esc_textarea($text)
+	{
+		$safe_text = htmlspecialchars( $text, ENT_QUOTES );
+		return $safe_text;
+	}
+}
+
+if(!function_exists('xyz_smap_plugin_get_version'))
+{
+	function xyz_smap_plugin_get_version()
+	{
+		if ( ! function_exists( 'get_plugins' ) )
+			require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		$plugin_folder = get_plugins( '/' . plugin_basename( dirname( XYZ_SMAP_PLUGIN_FILE ) ) );
+		// 		print_r($plugin_folder);
+		return $plugin_folder['social-media-auto-publish.php']['Version'];
+	}
+}
+
+
+if(!function_exists('xyz_smap_links')){
+	function xyz_smap_links($links, $file) {
+		$base = plugin_basename(XYZ_SMAP_PLUGIN_FILE);
+		if ($file == $base) {
+
+			$links[] = '<a href="http://xyzscripts.com/support/" class="xyz_support" title="Support"></a>';
+			$links[] = '<a href="http://twitter.com/xyzscripts" class="xyz_twitt" title="Follow us on twitter"></a>';
+			$links[] = '<a href="https://www.facebook.com/xyzscripts" class="xyz_fbook" title="Facebook"></a>';
+			$links[] = '<a href="https://plus.google.com/101215320403235276710/" class="xyz_gplus" title="+1"></a>';
+		}
+		return $links;
+	}
+}
+add_filter( 'plugin_row_meta','xyz_smap_links',10,2);
+
+?>
